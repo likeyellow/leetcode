6ba111e37886580b4ce399e9c8f1c4c50fbfd529
BAE EUNMI <likeyellowand@gmail.com>
2023-05-26 21:56:28 +0900

    Time: 3 ms, Memory: 44.7 MB - LeetHub

--- 0max-consecutive-ones/0max-consecutive-ones.java
+++ 0max-consecutive-ones/0max-consecutive-ones.java
@@ -1,15 +1,19 @@
 class Solution {
-    public int findMaxConsecutiveOnes(int[] nums) {
+    public int findMaxConsecutiveOnes(int[] nums) { // 연속된 1의 최대개수는 몇개인지 출력
+        
         int count = 0;
-        int result = 0;
-        for(int i=0; i < nums.length; i++) {
-            if (nums[i] == 1) {
+        int maxCount = 0; 
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 1) {
                 count ++;
-            } else {
-                result = Math.max(result, count);
+                maxCount = Math.max(maxCount, count);
+                
+            }else if(nums[i] == 0) {
                 count = 0;
             }
         }
-        return Math.max(result, count);
+        
+        return maxCount;
     }
 }
