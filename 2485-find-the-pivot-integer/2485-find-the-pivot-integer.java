607fc67019fcd2684d167d6663ec56dbc5f9113e
BAE EUNMI <likeyellowand@gmail.com>
2024-03-15 14:38:05 +0900

    Time: 1 ms (65.27%), Space: 41 MB (14.05%) - LeetHub

--- 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
+++ 2485-find-the-pivot-integer/2485-find-the-pivot-integer.java
@@ -10,9 +10,9 @@ class Solution {
         
         for(int i = n/2; i < n-1; i++) {
             int sum1 = preSum[i-1];
-            int sum2 = preSum[n] - preSum[i];
-            
-            if(sum1 == sum2) {
+            int sum2 = preSum[n] - preSum[i]; // preSum[n]: 1~n까지 숫자들의 합
+                                              // preSum[i]: 1~i까지 숫자들의 합      
+            if(sum1 == sum2) {                // 따라서 sum2 는 i 이후의 숫자들의 합
                 return i;
             } 
         }
