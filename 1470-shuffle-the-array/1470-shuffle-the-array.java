3aff4553d8d74a6b4b72d7c3915b6628c009d6fd
BAE EUNMI <likeyellowand@gmail.com>
2024-03-24 22:06:17 +0900

    Time: 0 ms (100.00%), Space: 44.4 MB (72.20%) - LeetHub

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+        int[] result = new int[n * 2];
+        
+        for(int i = 0; i < n; i++) {
+            result[2 * i] = nums[i];
+            result[2 * i + 1] = nums[n + i];
+        }
+        return result;
+        
+    }
+}
