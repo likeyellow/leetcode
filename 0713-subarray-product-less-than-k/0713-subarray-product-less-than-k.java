83438615d9b0c0085e207b7ddea0dd4390373bab
BAE EUNMI <likeyellowand@gmail.com>
2024-03-28 18:43:25 +0900

    Time: 4 ms (100.00%), Space: 47.7 MB (49.39%) - LeetHub

---
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int cnt = 0;
+        int total = 1;
+        int left = 0;
+        int right = 0;
+        
+        if(k <= 1) return 0;
+        
+        while(right < nums.length) {
+
+            total *= nums[right];
+            while(total >= k) {
+                total /= nums[left++];
+            }             
+
+            cnt += right - left + 1;
+            right++;
+        }
+        return cnt;        
+    }
+}
