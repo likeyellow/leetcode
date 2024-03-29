9847cc28876326405975d7bb4b9acaf0999c9af6
BAE EUNMI <likeyellowand@gmail.com>
2024-03-29 20:58:00 +0900

    Time: 3 ms (26.80%), Space: 45.9 MB (62.41%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,5 +1,6 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        /*
         int find = 0;
         
         for(int i = 0; i < nums.length; i++) { 
@@ -10,6 +11,18 @@ class Solution {
         }
         for(int j = find; j < nums.length; j++) {
             nums[j] = 0;
+        }
+        */
+        int j = -1;
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0 && j == -1) { // 배열 처음이 0일 경우 처리
+                j = i;
+            } else if(nums[i] != 0 && j != -1) {
+                nums[j] = nums[i];
+                nums[i] = 0;
+                j++;
+            }
         }
     }
 }
