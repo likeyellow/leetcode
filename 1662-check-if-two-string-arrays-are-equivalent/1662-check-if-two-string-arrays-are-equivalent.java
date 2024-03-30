8ff244638d6be83decba5e59aae5f40166c31c44
BAE EUNMI <likeyellowand@gmail.com>
2024-03-30 23:18:51 +0900

    Time: 0 ms (100.00%), Space: 41.4 MB (64.15%) - LeetHub

---
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        Map<Character, Integer> map1 = new HashMap<>();
+        Map<Character, Integer> map2 = new HashMap<>();
+        
+        
+        StringBuilder sb1 = new StringBuilder();
+        for(int i = 0; i < word1.length; i++) {
+            sb1.append(word1[i]);
+        }
+        
+        StringBuilder sb2 = new StringBuilder();
+        for(int j = 0; j < word2.length; j++) {
+            sb2.append(word2[j]);
+        }
+        
+        boolean result = false;
+        if((sb1.toString()).equals(sb2.toString())) {
+            result = true;
+        }
+        return result;
+    }    
+}
