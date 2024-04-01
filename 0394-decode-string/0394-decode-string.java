d4f89a50503c8dd27c3830f119f0ce272acfca03
BAE EUNMI <likeyellowand@gmail.com>
2024-04-01 17:51:40 +0900

    Time: 2 ms (38.04%), Space: 41.3 MB (79.63%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String decodeString(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            
+            if(ch == ']') {
+                List<Character> decode = new ArrayList<>();
+                
+                while(stack.peek() != '[') {
+                    decode.add(stack.pop());
+                }
+                // pop [ from the stack
+                stack.pop();
+                
+                int base = 1;
+                int k = 0;
+                // get the number k
+                while(!stack.isEmpty() && Character.isDigit(stack.peek())) {
+                    k = k + (stack.pop() - '0') * base;
+                    base *= 10;                    
+                }
+                
+                // pushing k times into stack
+                while(k != 0) {
+                    for(int j = decode.size() - 1; j >= 0; j--) {
+                        stack.push(decode.get(j));
+                    }
+                    k--;
+                }
+            } else { // push the current character to stack
+                stack.push(s.charAt(i));
+            }
+        }
+        // get the result from stack
+        char[] result = new char[stack.size()];
+        for(int i = result.length -1; i>= 0; i--) {
+            result[i] = stack.pop();
+        }
+        return new String(result);
+
+    }
+}
