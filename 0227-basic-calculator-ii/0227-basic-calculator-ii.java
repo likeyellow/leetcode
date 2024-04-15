1634759409e26812265b61b81c110a760052c828
BAE EUNMI <likeyellowand@gmail.com>
2024-04-15 10:19:54 +0900

    Time: 27 ms (28.76%), Space: 46.3 MB (39.99%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,5 +1,6 @@
 class Solution {
     public int calculate(String s) {
+        /*
         int ans = 0;
         int lastNum = 0, totalNum = 0;
         char oper = '+';
@@ -29,6 +30,36 @@ class Solution {
                 oper = ch;
                 lastNum = 0;
             }
+        }
+        return ans;
+        */
+        if(s == null || s.isEmpty()) return 0;
+        
+        Stack<Integer> stack = new Stack<>();
+        int lastNum = 0;
+        char oper = '+';
+        for(int i = 0; i < s.length(); i++) {
+            char lastChar = s.charAt(i);
+            if(Character.isDigit(lastChar)) {
+                lastNum = lastNum * 10 + (lastChar - '0');
+            }
+            if(!Character.isDigit(lastChar) && !Character.isWhitespace(lastChar) || i == s.length() -1) {
+                if(oper == '-') {
+                    stack.push(-lastNum);
+                } else if(oper == '+') {
+                    stack.push(lastNum);
+                } else if(oper == '*') {
+                    stack.push(stack.pop() * lastNum);
+                } else if(oper == '/') {
+                    stack.push(stack.pop() / lastNum);
+                }
+                oper = lastChar;
+                lastNum = 0;
+            }
+        }
+        int ans = 0;
+        while(!stack.isEmpty()) {
+            ans += stack.pop();
         }
         return ans;
     }
